c27a7dfb0cce425e29f1d2ad65ff593abad374ed
jose-almir <jr.cod.dev@gmail.com>
2023-03-13 11:56:47 -0300

    Montagem do index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Trazer de outro arquivo/biblioteca alguma função ou variável
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Selecionando a div na página
+const rootElement = document.getElementById("root");
+
+// A div root será a base da aplicação
+const root = createRoot(rootElement);
+
+// Inserimos o conteúdo da página
+
+let nome = "Almir";
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <section>
+    <h1>React é top</h1>
+    <img src="https://picsum.photos/200" />
+    <p>
+      Olá, tudo bem? <br /> <b>{nome.toUpperCase()}</b>
+    </p>
+    <p>Soma: {1 + 1}</p>
+  </section>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// JSX = Javascript Extension
+// No fim o HTML é convertido para Javascript (por baixo dos panos)
+// JSX = Açúcar Sintático
+
+// Dentro das chaves podemos escrever expressões JS!
